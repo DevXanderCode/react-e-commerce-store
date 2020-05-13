c4314c2f5545f75cefc2cdf94af071104c559279
Chinedu Alex <nwekealexchinedu@gmail.com>
2020-05-13 18:38:53 +0100

    Updates

---
+++ precache-manifest.4b4d34de3a072e147f8163c31c563be1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e6782836a2bdcb81dc2d8da5963a488",
+    "url": "/react-e-commerce-store/index.html"
+  },
+  {
+    "revision": "a8212e526f4e26d1f2c8",
+    "url": "/react-e-commerce-store/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "d33f26697311fc90077b",
+    "url": "/react-e-commerce-store/static/css/main.b01edffe.chunk.css"
+  },
+  {
+    "revision": "a8212e526f4e26d1f2c8",
+    "url": "/react-e-commerce-store/static/js/2.51338350.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-e-commerce-store/static/js/2.51338350.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d33f26697311fc90077b",
+    "url": "/react-e-commerce-store/static/js/main.510f33d2.chunk.js"
+  },
+  {
+    "revision": "486786763ac6cfdb60de",
+    "url": "/react-e-commerce-store/static/js/runtime-main.eabf5f43.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/react-e-commerce-store/static/media/logo.9f7e506d.svg"
+  }
+]);
